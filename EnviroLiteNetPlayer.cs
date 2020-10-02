e39462e0a7346f81d354e84d3eb0953e68649d62
LeafRockJ <71341252+LeafRockJ@users.noreply.github.com>
2020-10-02 15:18:54 +0300

    Initial commit

---
+++ EnviroLiteNetPlayer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLib;
+using LiteNetLibManager;
+using EnviroSamples;
+
+public class EnviroLiteNetPlayer : LiteNetLibBehaviour
+{
+
+    public bool assignOnStart = true;
+    public bool findSceneCamera = true;
+
+    public GameObject Player;
+    public Camera PlayerCamera;
+
+    public override void OnSetup()
+    {
+        base.OnSetup();
+        RegisterNetFunction(Cmd_RequestSeason);
+        RegisterNetFunction(Cmd_RequestCurrentWeather);
+        RegisterNetFunction<int, int>(RpcRequestCurrentWeather);
+        RegisterNetFunction<int>(RpcRequestSeason);
+
+        RegisterNetFunction<string>(Cmd_RequestWeatherChange);      // ADDED
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Deactivate if it isn't ours!
+        if (!IsOwnerClient && !IsServer)
+        {
+            this.enabled = false;
+            return;
+        }
+
+        if (PlayerCamera == null && findSceneCamera)
+        {
+            StartCoroutine(FindIt());
+        }
+
+        if (IsOwnerClient)
+        {
+            if (assignOnStart && Player != null && PlayerCamera != null)
+                EnviroSkyMgr.instance.AssignAndStart(Player, PlayerCamera);
+
+            CallNetFunction(Cmd_RequestSeason, FunctionReceivers.Server);
+            CallNetFunction(Cmd_RequestCurrentWeather, FunctionReceivers.Server);
+        }
+    }
+
+    public IEnumerator FindIt()
+    {
+        yield return new WaitForSeconds(3);
+        PlayerCamera = Camera.main;
+    }
+
+    private void Cmd_RequestSeason()
+    {
+        RpcRequestSeason((int)EnviroSkyMgr.instance.Seasons.currentSeasons);
+    }
+
+    private void RpcRequestSeason(int season)
+    {
+        EnviroSkyMgr.instance.ChangeSeason((EnviroSeasons.Seasons)season);
+    }
+
+    public void Cmd_RequestWeatherChange(string id)      // ADDED
+    {
+        EnviroSkyMgr.instance.ChangeWeather(id);
+    }
+
+    public void RequestWeatherChange(string weather)
+    {
+        CallNetFunction(Cmd_RequestWeatherChange, FunctionReceivers.Server, weather);
+    }
+
+    private void Cmd_RequestCurrentWeather()
+    {
+        for (int i = 0; i < EnviroSkyMgr.instance.Weather.zones.Count; i++)
+        {
+            for (int w = 0; w < EnviroSkyMgr.instance.Weather.WeatherPrefabs.Count; w++)
+            {
+                if (EnviroSkyMgr.instance.Weather.WeatherPrefabs[w] == EnviroSkyMgr.instance.Weather.zones[i].currentActiveZoneWeatherPrefab)
+                    RpcRequestCurrentWeather(w, i);
+            }
+        }
+    }
+
+    private void RpcRequestCurrentWeather(int weather, int zone)
+    {
+        EnviroSkyMgr.instance.Weather.zones[zone].currentActiveZoneWeatherPrefab = EnviroSkyMgr.instance.Weather.WeatherPrefabs[weather];
+        EnviroSkyMgr.instance.Weather.zones[zone].currentActiveZoneWeatherPreset = EnviroSkyMgr.instance.Weather.WeatherPrefabs[weather].weatherPreset;
+    }
+}
---
+++ EnviroLiteNetServer.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EnviroSamples;
+//using MultiplayerARPG;
+using LiteNetLib;
+using LiteNetLibManager;
+
+public class EnviroLiteNetServer : LiteNetLibBehaviour
+{
+
+    public float updateSmoothing = 15f;
+    public int zoneID = 0;
+    [SerializeField]
+    public LiteNetLibSyncField<float> networkHours = new LiteNetLibSyncField<float>();
+    [SerializeField]
+    public LiteNetLibSyncField<int> networkDays = new LiteNetLibSyncField<int>();
+    [SerializeField]
+    public LiteNetLibSyncField<int> networkYears = new LiteNetLibSyncField<int>();
+
+    public bool isHeadless = true;
+
+    public override void OnSetup()
+    {
+        if (!isHeadless)
+        {
+            EnviroSkyMgr.instance.StartAsServer();
+        }
+
+        EnviroSkyMgr.instance.Weather.updateWeather = true;
+
+        RegisterNetFunction<EnviroSeasons.Seasons>(SendSeasonToClient);
+        RegisterNetFunction<EnviroWeatherPreset, EnviroZone>(SendWeatherToClient);
+
+        EnviroSkyMgr.instance.OnSeasonChanged += (EnviroSeasons.Seasons season) =>
+        {
+            CallNetFunction(SendSeasonToClient, FunctionReceivers.All, season);
+        };
+        EnviroSkyMgr.instance.OnZoneWeatherChanged += (EnviroWeatherPreset type, EnviroZone zone) =>
+        {
+            CallNetFunction(SendWeatherToClient, FunctionReceivers.All, type, zone);
+        };
+
+        RegisterNetFunction<int>(RpcSeasonUpdate);
+        RegisterNetFunction<int, int>(RpcWeatherUpdate);
+
+    }
+
+    // Use this for initialization
+    public void Start()
+    {
+
+    }
+
+    void SendWeatherToClient(EnviroWeatherPreset w, EnviroZone z)
+    {
+
+        for (int i = 0; i < EnviroSkyMgr.instance.Weather.zones.Count; i++)
+        {
+            if (EnviroSkyMgr.instance.Weather.zones[i] == z)
+                zoneID = i;
+
+        }
+
+        for (int i = 0; i < EnviroSkyMgr.instance.Weather.weatherPresets.Count; i++)
+        {
+
+            if (EnviroSkyMgr.instance.Weather.weatherPresets[i] == w)
+                RpcWeatherUpdate(i, zoneID);
+        }
+    }
+
+    void SendSeasonToClient(EnviroSeasons.Seasons s)
+    {
+        RpcSeasonUpdate((int)s);
+    }
+
+    void RpcSeasonUpdate(int season)
+    {
+        EnviroSkyMgr.instance.ChangeSeason((EnviroSeasons.Seasons)season);
+    }
+
+    void RpcWeatherUpdate(int weather, int zone)
+    {
+        EnviroSkyMgr.instance.Weather.zones[zone].currentActiveZoneWeatherPrefab = EnviroSkyMgr.instance.Weather.WeatherPrefabs[weather];
+        EnviroSkyMgr.instance.Weather.zones[zone].currentActiveZoneWeatherPreset = EnviroSkyMgr.instance.Weather.WeatherPrefabs[weather].weatherPreset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EnviroSkyMgr.instance == null)
+            return;
+
+        if (!IsServer)
+        {
+            if (networkHours.Value < 1f && EnviroSkyMgr.instance.GetUniversalTimeOfDay() > 23f)
+                EnviroSkyMgr.instance.SetTimeOfDay(networkHours.Value);
+
+            EnviroSkyMgr.instance.SetTimeOfDay(Mathf.Lerp(EnviroSkyMgr.instance.GetUniversalTimeOfDay(), networkHours.Value, Time.deltaTime * updateSmoothing));
+            EnviroSkyMgr.instance.Time.ProgressTime = EnviroTime.TimeProgressMode.Simulated;
+
+        }
+        networkHours.Value = EnviroSkyMgr.instance.GetUniversalTimeOfDay();
+        EnviroSkyMgr.instance.OnZoneWeatherChanged += (EnviroWeatherPreset type, EnviroZone zone) =>
+        {
+            CallNetFunction(SendWeatherToClient, FunctionReceivers.All, type, zone);
+        };
+    }
+}
